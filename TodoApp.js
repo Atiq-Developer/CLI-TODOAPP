4d32ca15f076ef2fb7f66c1d7271475fe9d5e78f
Muhammad Atiq Ur Rehman <146697001+Atiq-Developer@users.noreply.github.com>
2024-05-05 20:43:03 +0500

    CLI Based TodoAPP

---
+++ TodoApp.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let tasks = [];
+let condition = true;
+while (condition) {
+    let answer = await inquirer.prompt([
+        {
+            message: "What you want to do add more sports in your todolist?",
+            type: "input",
+            name: "addTodos",
+        },
+        {
+            message: "Do you want to add more todo?",
+            type: "confirm",
+            name: "addMoreTodos",
+            default: "false",
+        },
+    ]);
+    tasks.push(answer.addTodos);
+    condition = answer.addMoreTodos;
+    console.log(tasks);
+}
